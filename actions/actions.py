341c845499e49283cd8898160ee1bf93492be758
Liafau <58419564+Liafau@users.noreply.github.com>
2021-07-27 10:47:52 +0700

    comment action

--- actions/actions.py
+++ actions/actions.py
@@ -26,26 +26,26 @@
 #
 #         return []
 
-from typing import Any, Text, Dict, List
+# from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.events import UserUtteranceReverted
-from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.events import UserUtteranceReverted
+# from rasa_sdk.executor import CollectingDispatcher
 
-class ActionDefaultFallback(Action):
-    """Executes the fallback action and goes back to the previous state
-    of the dialogue"""
+#class ActionDefaultFallback(Action):
+#    """Executes the fallback action and goes back to the previous state
+#    of the dialogue"""
 
-    def name(self) -> Text:
-        return ACTION_DEFAULT_FALLBACK_NAME
+#    def name(self) -> Text:
+#        return ACTION_DEFAULT_FALLBACK_NAME
 
-    async def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="my_custom_fallback_template")
+#    async def run(
+#        self,
+#        dispatcher: CollectingDispatcher,
+#        tracker: Tracker,
+#        domain: Dict[Text, Any],
+#    ) -> List[Dict[Text, Any]]:
+#        dispatcher.utter_message(template="my_custom_fallback_template")
 
         # Revert user message which led to fallback.
-        return [UserUtteranceReverted()]
+        #return [UserUtteranceReverted()]
